2a2ab276202b94a7ceaf6907bd0386f175e6822c
Samiksha1618 <samikshathakre168@gmail.com>
2025-06-19 20:45:18 +0530

    Added Week_1 folder

---
+++ Week_1/Week 1_Design Patterns and Principles/Implementing the Singleton Pattern/code/TestSingleton.java
@@ -0,0 +1,16 @@
+public class TestSingleton {
+    public static void main(String[] args) {
+        Logger logger1 = Logger.getInstance();
+        logger1.log("First message");
+
+        Logger logger2 = Logger.getInstance();
+        logger2.log("Second message");
+
+        
+        if (logger1 == logger2) {
+            System.out.println("Both logger1 and logger2 are the same instance.");
+        } else {
+            System.out.println("Different instances were created.");
+        }
+    }
+}
